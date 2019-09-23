f0c33aec7d548b5745e1d202b860783604662ed0
Simon Ordish <simon.ordish@masagi.co.uk>
2019-09-23 14:19:26 +0100

    site

---
+++ precache-manifest.36e626e866a464cb56c1a1c3607dd399.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e0ae2eddf28e2d029257b0abc588330",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5388246223eb16ce58b2",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "748a0f48313fbcae1c4f",
+    "url": "/static/js/2.16789526.chunk.js"
+  },
+  {
+    "revision": "5388246223eb16ce58b2",
+    "url": "/static/js/main.ec1c5260.chunk.js"
+  },
+  {
+    "revision": "252c4efe63d7755e63af",
+    "url": "/static/js/runtime-main.e24d10e1.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
